a45457846317d0206e9b0f8e48602fbfeb2fda91
tchmr <48376551+tchmr@users.noreply.github.com>
2022-12-30 07:05:07 +0000

    update: エラー解消 all

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
   belongs_to :user
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  
+  def posted_by?(user)
+    self&.user_id == user&.id
+  end
 end
