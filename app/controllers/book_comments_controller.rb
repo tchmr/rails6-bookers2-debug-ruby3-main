4d0ab0b99496d4075918546ec91e2b7eb75c7964
tchmr <48376551+tchmr@users.noreply.github.com>
2023-01-02 09:40:37 +0000

    update: コメント機能非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,15 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_back fallback_location: root_path
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.find_by(book: book, id: params[:id])
+    @book = Book.find(params[:book_id])
+    comment = current_user.book_comments.find_by(book: @book, id: params[:id])
     comment.destroy
-    redirect_back fallback_location: root_path
   end
 
   private
@@ -19,7 +17,7 @@ class BookCommentsController < ApplicationController
   def book_comment_params
     params.require(:book_comment).permit(:body)
   end
-  
+
   def ensure_correct_user
     comment = BookComment.find(params[:id])
     unless comment.user == current_user
