7b5954d0a4173e4158af4ed72ed228aff247f11a
tchmr <48376551+tchmr@users.noreply.github.com>
2023-01-03 00:44:17 +0000

    update: いいねソート機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @books = @user.books.sorted_by_favorites
     @book = Book.new
   end
 
