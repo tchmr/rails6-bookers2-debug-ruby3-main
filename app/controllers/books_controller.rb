bccb13ef90cec7eb1152a6dd3fb62316e323f76d
Yosuke Tsuchiya <48376551+tchmr@users.noreply.github.com>
2023-01-07 08:40:16 +0900

    投稿数レポート表示機能 (#8)
    * update: 記録表のviewモック作成
    
    * update: レポート機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :ensure_correct_user, only: [:update, :edit, :create, :destroy]
+  before_action :ensure_correct_user, only: [:update, :edit, :destroy]
 
   def show
     @book = Book.find(params[:id])
@@ -48,7 +48,7 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
   def ensure_correct_user
     @book = Book.find(params[:id])
     unless @book.user == current_user
