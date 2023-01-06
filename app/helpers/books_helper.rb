956d4c03bb921cefa8c5f6b6af41460dce6a0160
tchmr <48376551+tchmr@users.noreply.github.com>
2023-01-06 23:38:49 +0000

    update: レポート機能実装

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,18 @@
 module BooksHelper
+  Time.zone = 'Asia/Tokyo'
+  
+  # 比率を求める
+  def culc_rate(current_count, previous_count)
+    return 0 if previous_count == 0
+    rate = current_count.to_f / previous_count
+    (rate * 100).to_i
+  end
+  
+  def all_last_week
+    (Time.zone.now.ago(1.week + 6.days).beginning_of_day)..(Time.zone.now.ago(1.week).end_of_day)
+  end
+  
+  def all_current_week
+    (Time.zone.now.ago(6.days).beginning_of_day)..(Time.zone.now.end_of_day)
+  end
 end
